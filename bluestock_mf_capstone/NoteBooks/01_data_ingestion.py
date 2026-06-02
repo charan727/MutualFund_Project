2a7bbcbe9c99fcbdf3a90f3893d949333d37a717
charan727 <charanchinthala42@gmail.com>
2026-06-02 21:40:11 +0530

    Create 01_data_ingestion.py

---
+++ bluestock_mf_capstone/NoteBooks/01_data_ingestion.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import os
+
+data_path = "Data/raw"
+
+files = [
+    "01_fund_master.csv",
+    "02_nav_history.csv",
+    "03_aum_by_fund_house.csv",
+    "04_monthly_sip_inflows.csv",
+    "05_category_inflows.csv",
+    "06_industry_folio_count.csv",
+    "07_scheme_performance.csv",
+    "08_investor_transactions.csv",
+    "09_portfolio_holdings.csv",
+    "10_benchmark_indices.csv"
+]
+
+for file in files:
+    print("\n" + "="*80)
+    print(f"Dataset: {file}")
+    print("="*80)
+
+    df = pd.read_csv(os.path.join(data_path, file))
+
+    print("\nShape:")
+    print(df.shape)
+
+    print("\nData Types:")
+    print(df.dtypes)
+
+    print("\nFirst 5 Rows:")
+    print(df.head())
+
+    print("\nMissing Values:")
+    print(df.isnull().sum())
+
+    print("\nDuplicate Records:")
+    print(df.duplicated().sum())
